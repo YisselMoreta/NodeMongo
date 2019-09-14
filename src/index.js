d1deed125f0982e94a22e225cfbecc49505757ae
Laura <luaana788@gmail.com>
2019-09-14 14:15:28 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require ('express');
+const path = require ('path');
+const exphbs= require ('express-handlebars');
+const methodOverride= require ('method-override');
+const session= require ('express-session');
+
+
+
+//Initializations
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 3000 );
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs ({
+    defaultLayaout: 'main' ,
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//Middlewares
+
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUnitialized: true,
+    
+    
+}));
+
+
+
+//Global variaables 
+
+//Routes
+
+app.get('')
+
+//Static files
+
+//Server is listenning
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port ', app.get('port'));
+});
