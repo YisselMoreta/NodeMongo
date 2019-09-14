94ea032e9b6fc2f1877b95555d3ced35bbb36917
Laura <luaana788@gmail.com>
2019-09-14 19:39:21 +0200

    notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/Note');
+
+router.get('/notes/add', (req, res)=>{
+    res.render('notes/newnote');
+    
+});
+router.post('/notes/newnote',  (req, res)=>{
+   const {title, description} = req.body;
+   const errors = [];
+   
+   if (!title){
+       errors.push({text: 'insterta un titulo!'});
+       
+   }
+    if (!description){
+       errors.push({text: 'insterta una description!'});
+       
+   }
+   if (errors.length > 0){
+       
+       res.render('notes/newnote', {
+           errors,
+           title,
+           description
+    });
+       
+   } else {
+    const newNote = new Note({title, description});
+    console.log(newNote);
+    res.send('ok');
+   }
+    
+});
+
 router.get('/notes', (req, res)=>{
-    res.send('notes');
+    res.send('Notes from database');
 });
 
 
